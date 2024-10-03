0e870e49eb94a01c72fbadc60026728e646c3c69
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-10-03 16:24:07 +0000

    eslint checks

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,38 +1,37 @@
 export default class Currency {
-    constructor(code, name) {
-      this._code = code;
-      this._name = name;
-    }
-  
-    // Getter for code
-    get code() {
-      return this._code;
-    }
-  
-    // Setter for code
-    set code(value) {
-      if (typeof value !== 'string') {
-        throw new TypeError('Code must be a string');
-      }
-      this._code = value;
-    }
-  
-    // Getter for name
-    get name() {
-      return this._name;
-    }
-  
-    // Setter for name
-    set name(value) {
-      if (typeof value !== 'string') {
-        throw new TypeError('Name must be a string');
-      }
-      this._name = value;
-    }
-  
-    // Method to display full currency in the format "name (code)"
-    displayFullCurrency() {
-      return `${this._name} (${this._code})`;
-    }
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
   }
-  
+
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  // Setter for code
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  // Method to display full currency in the format "name (code)"
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
