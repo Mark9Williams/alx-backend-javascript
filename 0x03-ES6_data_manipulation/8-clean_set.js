9f8484529ef1363a369d2d99b12d74e8573fc68b
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-10-08 16:21:33 +0000

    corrections

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,14 @@
 export default function cleanSet(set, startString) {
-  if (typeof set !== 'object' || typeof startString !== 'string') {
+  // Check if startString is an empty string
+  if (!startString || typeof startString !== 'string') {
     return '';
   }
-  if (startString === '') {
-    return '';
-  }
-  const newSet = new Set();
-  set.forEach((value) => {
-    if (value.startsWith(startString)) {
-      newSet.add(value.slice(startString.length));
-    }
-  });
-  return [...newSet].join('-');
+
+  // Filter the values in the set that start with startString and extract the rest of the string
+  const result = [...set]
+    .filter((value) => value.startsWith(startString)) // only values that start with startString
+    .map((value) => value.slice(startString.length)) // remove the startString part
+    .join('-'); // join with '-'
+
+  return result;
 }
