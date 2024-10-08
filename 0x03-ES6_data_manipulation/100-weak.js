9e017a1600dbf3008002ba2da89672eda606c15d
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-10-08 17:10:53 +0000

    error on checking count

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,16 @@
+// Export the instance of WeakMap
 export const weakMap = new WeakMap();
+
+// Export the queryAPI function
 export function queryAPI(endpoint) {
+  // Check if the endpoint already exists in the WeakMap
   const currentCount = weakMap.get(endpoint) || 0;
 
+  // Increment the count for this endpoint
   weakMap.set(endpoint, currentCount + 1);
 
-  if (currentCount >= 5) {
+  // If the number of queries reaches or exceeds 5, throw an error
+  if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
 }
